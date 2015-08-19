3193b8a7557bf8bf12c6b250d6d6ec7032f4bc80
Joseph Livecchi <joewashear007@gmail.com>
2015-08-19 01:57:13 +0000

    Hacky main solution, basic weather data working

--- TempeScopeRPI/__main__.py
+++ TempeScopeRPI/__main__.py
@@ -1,36 +1,45 @@
 from weather import GetWeather
-import time
-import fan
-import mister
-import pump
-import light
+from mister import Device
 import RPi.GPIO as GPIO
+import time
 
+class Ctrl:
+	def __init__(self):
+		GPIO.setmode(GPIO.BOARD)
+		self.mister = Device("Mister", 7)
+		self.fan = Device("Fan", 15)
+		self.pump = Device("Pump", 11)
+
+	def main(self):
+		w = GetWeather()
+		self.processWeather(w)
+
+	def processWeather(self, w):
+		clouds = w['clouds'] /4
+		print("Clouds: ", clouds)
+		self.mister.power(True)
+		self.fan.power(True)
+		rain = 0
+		if "3h" in w['rain']:
+			rain = w['rain']['3h']
+
+		for i in range(1,25):
+			self.fan.power((not self.fan.on) and self.mister.on)
+			if i > clouds:
+				self.mister.power(False)
+			if i < rain :
+				self.pump.power(True)
+			else:
+				self.pump.power(False)
+			time.sleep(1)
+
+	def __del__(self):
+		GPIO.cleanup()
+
+		
 def main():
-	w = GetWeather()
-	print(w)
-	mister.On()
-	fan.On()
-	pump.On()
-	light.On()
-	time.sleep(5)
-	mister.Off()
-	fan.Off()
-	pump.Off()
-	light.Off()
-
-def startup():
-	GPIO.setmode(GPIO.BOARD)
-	fan.Setup()
-	pump.Setup()
-	mister.Setup()
-	light.Setup()
-
-def shutdown():
-	GPIO.cleanup()
-
+	ctrl = Ctrl()
+	ctrl.main()
 
 if __name__ == "__main__":
-	startup()
 	main()
-	shutdown()
--- TempeScopeRPI/mister.py
+++ TempeScopeRPI/mister.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 
 PIN = 7
+FULL = 10
 
 def Setup():
 	GPIO.setup(PIN, GPIO.OUT)
@@ -12,3 +13,20 @@ def On():
 def Off():
 	print("Mister Off")
 	GPIO.output(PIN, False)
+
+def play(val):
+	timeon = FULL * val / 100;
+	# ???
+	timeoff = timeon
+
+class Device:
+	def __init__(self, name, pin):
+		self.name = name
+		self.pin = pin
+		self.on = False
+		GPIO.setup(pin, GPIO.OUT)
+	def power(self, on):
+		self.on = on
+		print(self.name, " => ", self.on)
+		GPIO.output(self.pin, self.on)
+
