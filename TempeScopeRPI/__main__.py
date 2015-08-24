67482e9143b591998c9209f984d4a7d09069e8a9
Joseph Livecchi <joewashear007@gmail.com>
2015-08-23 22:26:25 -0400

    Integrated scheduler with main

--- TempeScopeRPI/__main__.py
+++ TempeScopeRPI/__main__.py
@@ -1,6 +1,7 @@
 from weather import GetWeather
 from device import Device
 import time
+import scheduler
 
 # Simulation
 # ---------------------------------------
@@ -8,14 +9,11 @@ simMode = True
 if not simMode:
     import RPi.GPIO as GPIO
 
-
 class Ctrl:
     def __init__(self):
         if not simMode:
             GPIO.setmode(GPIO.BOARD)
-        self.mister = Device("Mister", 7, simMode)
-        self.fan = Device("Fan", 15, simMode)
-        self.pump = Device("Pump", 11, simMode)
+        self.devices = [Device("Mister", 7, simMode), Device("Fan", 15, simMode), Device("Pump", 11, simMode)]
 
     def main(self):
         w = GetWeather()
@@ -24,29 +22,31 @@ class Ctrl:
     def processWeather(self, w):
         clouds = w['clouds'] /4
         print("Clouds: ", clouds)
-        self.mister.power(True)
-        self.fan.power(True)
-        rain = 0
-        if "3h" in w['rain']:
-            rain = w['rain']['3h']
-
-        for i in range(1,25):
-            self.fan.power((not self.fan.on) and self.mister.on)
-            if i > clouds:
-                self.mister.power(False)
-            if i < rain :
-                self.pump.power(True)
-            else:
-                self.pump.power(False)
-            time.sleep(1)
+        scheduler.schedule(self)
+        # rain = 0
+        # if "3h" in w['rain']:
+        #     rain = w['rain']['3h']
+        #
+        # for i in range(1,25):
+        #     self.fan.power((not self.fan.on) and self.mister.on)
+        #     if i > clouds:
+        #         self.mister.power(False)
+        #     if i < rain :
+        #         self.pump.power(True)
+        #     else:
+        #         self.pump.power(False)
+        #     time.sleep(1)
 
     def __del__(self):
         if not simMode:
             GPIO.cleanup()
 
 def main():
-    ctrl = Ctrl()
-    ctrl.main()
+    try:
+        ctrl = Ctrl()
+        ctrl.main()
+    finally:
+        scheduler.cleanUp()
 
 if __name__ == "__main__":
     main()
--- TempeScopeRPI/scheduler.py
+++ TempeScopeRPI/scheduler.py
@@ -1,48 +1,46 @@
-import sched, time, random, threading
+import sched
+import time
+import random
 from multiprocessing import Process
 
-q = 0
 s = sched.scheduler(time.time, time.sleep)
 threads = []
-devices = ["pump", "mister", "fan"]
 
-def m(q):
+def schedule(ctrl):
     cleanUp()
-    print("Running Master Loop")
     threads[:] = []
-    for key in devices:
-        q = q + 1
+    timeMax = 0
+    print('---------------------------------------------------')
+    for device in ctrl.devices:
         on = random.uniform(0.5,5)
         off = random.uniform(0.5,5)
-        z = Process(target=cycle, args=(key, on,off, q))
+        z = Process(target=cycle, args=(device, on, off))
         z.daemon = True
         threads.append(z)
+        print(' =>', device.name, ' - On:', on, '/ Off:', off)
+        timeMax = max(timeMax, on + off)
         z.start()
-    # time.sleep(5)
-    s.enter(30, 1, m, (q, ))
+    print(' => Time Loop: ', timeMax, ' Cycles:', (30 / timeMax))
+    print('---------------------------------------------------')
+    s.enter(30, 1, schedule, (ctrl, ))
     s.run()
-    # s = threading.Timer(5, m, (q,))
-    # s.start()
-    # m(q)
 
 def cleanUp():
     for t in threads:
         if t.is_alive():
             t.terminate()
             time.sleep(0.1)
-            print("Killed: ", q, " => ", t.is_alive(), t.exitcode)
-    # for t in threads:
+            print("Killed => ", t.is_alive(), t.exitcode)
 
-def cycle(key, on, off, q):
-    print(q, " => ", key , " - Active (", on ,")" )
+def cycle(device, on, off):
+    device.power(True)
     time.sleep(on)
-    print(q, " => ", key , " - Deactive (", off ,")" )
+    device.power(False)
     time.sleep(off)
-    cycle(key, on, off, q)
-    # s.enter(devices[i][x], 2, cycle, (i, not a))
+    cycle(device, on, off)
 
 if __name__ == "__main__":
     try:
-        m(0)
+        schedule(0)
     finally:
         cleanUp()
